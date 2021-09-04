268a7ecf083f23d4d5b72d4311976466a4926e75
SlavikChuprin <chuprin.00@yandex.ru>
2021-09-04 21:24:36 +0300

    create logic and paint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+ 
+
 import './sass/main.scss';
+import fetchCountries from './js/fetchCountries';
+import countriesListTpl from './templates/countriesList.hbs';
+import countryInfoTpl from './templates/countryInfo.hbs';
+const _ = require('lodash');
+
+
+const BASE_URL = 'https://restcountries.eu/rest/v2/';
+const refs={
+    input: document.querySelector('.input'),
+    countriesMarkUp: document.querySelector('.countries-result'),
+};
+
+refs.input.addEventListener('input',_.debounce(onSearch,500))
+
+function onSearch(e) {
+     const searchQuery= e.target.value;
+     console.log(searchQuery);
+    fetch(`${BASE_URL}name/${searchQuery}`)
+    .then(res=>res.json())
+    .then(renderCountriesList)
+    .catch(onFetchError); 
+};
+     
+console.dir();
+function renderCountriesList(countries){
+    if(countries.length ===1){
+ const list= countryInfoTpl(...countries);
+ refs.countriesMarkUp.innerHTML =list;
+    } else{
+        console.log("больше 1",countries.length );
+     const list= countriesListTpl(countries);
+     refs.countriesMarkUp.innerHTML =list;
+ };
+ refs.input.value="";
+};
+
+
+function onFetchError (error){
+    alert('Error');
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,43 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2/'
+export default function fetchCountries(searchQuery) {
+
+    fetch(`${BASE_URL}name/${searchQuery}`)
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+    
+};
+ 
+// refs.searchForm.addEventListener('submit', onSearch);
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   const form = e.currentTarget;
+//   const searchQuery = form.elements.query.value;
+
+//   API.fetchPokemon(searchQuery)
+//     .then(renderPokemonCard)
+//     .catch(onFetchError)
+//     .finally(() => form.reset());
+// }
+
+// function renderPokemonCard(pokemon) {
+//   const markup = pokemonCardTpl(pokemon);
+//   refs.cardContainer.innerHTML = markup;
+// }
+
+// function onFetchError(error) {
+//   alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+// }
+
+// // =========================================
+
+// const url = 'https://newsapi.org/v2/everything?q=cars';
+// const options = {
+//   headers: {
+//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+//   },
+// };
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
